75b0556a759ce9929a2c466645c2cc0f11fd7f54
Henrique Sabatini <henriquesabatini2016@gmail.com>
2024-06-11 08:08:43 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/HenriqueSabatini.png" />
-    <Titulo fontSize={20}>Henrique Sabatini</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Henrique Sabatini</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        HenriqueSabatini
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Melhorando
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
